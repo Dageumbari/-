c530aa4e1cef4b9e5300b348c770ff39165786c4
juyeong <juyg0628@gmail.com>
2020-06-04 22:45:04 +0900

    2020.06.04 채팅 테스트 코드

---
+++ FinalProject/FinalProject/src/main/java/com/bit/msgStompWebSocket/GreetingTest.java
@@ -0,0 +1,17 @@
+package com.bit.msgStompWebSocket;
+
+public class GreetingTest {
+	  private String content;
+
+	  public GreetingTest() {
+	  }
+
+	  public GreetingTest(String content) {
+	    this.content = content;
+	  }
+
+	  public String getContent() {
+	    return content;
+	  }
+
+}
