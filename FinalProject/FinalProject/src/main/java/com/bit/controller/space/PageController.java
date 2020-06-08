14f6b919e9b38636cb54545565be749de18e200a
juyeong <juyg0628@gmail.com>
2020-06-08 14:56:22 +0900

    채팅 restcontroller 수정

--- FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
+++ FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
@@ -1,20 +1,19 @@
-package com.bit.controller.space;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.bit.model.dao.PageDAO;
-
-@Controller
-public class PageController {
-	
-	@Autowired
-	PageDAO pageDAO;
-	
-	/*
-	 * @GetMapping("/pageTest") public String getPagebyPageNo(int PageNo) { return
-	 * "/space/pageTest"; }
-	 */
-
-}
+/*
+ * package com.bit.controller.space;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.stereotype.Controller;
+ * 
+ * import com.bit.model.dao.PageDAO;
+ * 
+ * @Controller public class PageController {
+ * 
+ * @Autowired PageDAO pageDAO;
+ * 
+ * 
+ * @GetMapping("/pageTest") public String getPagebyPageNo(int PageNo) { return
+ * "/space/pageTest"; }
+ * 
+ * 
+ * }
+ */
--- FinalProject/FinalProject/src/main/java/com/bit/model/vo/ChattingVO.java
+++ FinalProject/FinalProject/src/main/java/com/bit/model/vo/ChattingVO.java
@@ -1,6 +1,7 @@
 package com.bit.model.vo;
 
 import java.sql.Timestamp;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,4 +16,16 @@ public class ChattingVO {
 	private String chatContent;
 	private int adminCode;
 	private int userNo;
+	
+	public ChattingVO() {
+	}
+	public ChattingVO(int chatNo, String chatContent, int adminCode, int userNo )
+	{
+		this.chatNo = chatNo;
+		this.chatContent = chatContent;
+		this.adminCode = adminCode;
+		this.userNo = userNo;
+		this.msgGotTime=msgGotTime;
+		this.msgSendTime=msgSendTime;
+	}
 }
