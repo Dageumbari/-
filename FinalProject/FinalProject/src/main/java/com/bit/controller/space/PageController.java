57641a5a7cac90665a03f2b4f761bf577b6228f1
tokkisource <withtokki@gmail.com>
2020-06-09 19:18:13 +0900

    logback 오류수정

--- FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
+++ FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bit.model.service.PageService;
 
 import lombok.extern.java.Log;
-
 @Log
 @Controller
 public class PageController {
@@ -17,12 +15,14 @@ public class PageController {
 	@Autowired
 	PageService PageService;
 	
-	
-	
-	@GetMapping("/spaceTest")
-	public String getTestEditPage() {
-		return "space/test";
+
+	@GetMapping("/pageList")
+	public String getPageList(Model model) {
+		model.addAttribute("list", PageService.getPageList());	
+		return "space/pageList";
 	}
+	
+	
 	@GetMapping("page")
 	public String page() {
 		return "page/page";
--- FinalProject/FinalProject/src/main/java/com/bit/model/service/PageService.java
+++ FinalProject/FinalProject/src/main/java/com/bit/model/service/PageService.java
@@ -1,7 +1,9 @@
 package com.bit.model.service;
 
+import java.util.List;
+
 import com.bit.model.vo.PageVO;
 
 public interface PageService {
-	
+	public List<PageVO> getPageList();
 }
