02d0cca6b338cb31bb42da70d01f36e17c5d74e7
tokkisource <withtokki@gmail.com>
2020-06-10 14:19:17 +0900

    daoMapper_sample 내용삭제

--- FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
+++ FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
@@ -8,23 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.bit.model.service.PageService;
 
 import lombok.extern.java.Log;
+
 @Log
 @Controller
 public class PageController {
 
 	@Autowired
 	PageService PageService;
-	
 
 	@GetMapping("/pageList")
 	public String getPageList(Model model) {
-		model.addAttribute("list", PageService.getPageList());	
+		model.addAttribute("list", PageService.getPageList());
 		return "space/pageList";
 	}
-	
-	
+
 	@GetMapping("page")
 	public String page() {
 		return "page/page";
 	}
-	}
+}
