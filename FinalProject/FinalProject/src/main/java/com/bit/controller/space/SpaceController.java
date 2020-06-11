d50162b74903dfc7fd1b43a3cbfbfd6c8216ea49
tokkisource <withtokki@gmail.com>
2020-06-11 11:13:41 +0900

    h2삭제

--- FinalProject/FinalProject/src/main/java/com/bit/controller/space/SpaceController.java
+++ FinalProject/FinalProject/src/main/java/com/bit/controller/space/SpaceController.java
@@ -1,5 +1,22 @@
 package com.bit.controller.space;
 
-public class SpaceController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class SpaceController {
+	
+	@GetMapping("/sidebar")
+	public String sidebar() {
+		return "common/sidebar";
+	}
+	
+	@PostMapping("/spaceHello")
+	public String sayHello(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
 }
