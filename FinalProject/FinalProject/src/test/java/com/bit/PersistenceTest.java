dcdd3c01fd0bd827a337dd6c820c9e1e134d8275
yanchu0 <dustn0514@gmail.com>
2020-06-12 17:12:23 +0900

    project update시 생성된 head및 websocket pakage삭제

--- FinalProject/FinalProject/src/test/java/com/bit/PersistenceTest.java
+++ FinalProject/FinalProject/src/test/java/com/bit/PersistenceTest.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bit.model.dao.PageDAO;
-<<<<<<< HEAD
 
-=======
->>>>>>> 57641a5a7cac90665a03f2b4f761bf577b6228f1
 
 @SpringBootTest
 public class PersistenceTest {
