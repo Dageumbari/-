b3625309b57412b7014467ddb8f608535623e569
yanchu0 <dustn0514@gmail.com>
2020-06-15 17:34:39 +0900

    spacePage.html 오류사항 수정

--- FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
+++ FinalProject/FinalProject/src/main/java/com/bit/controller/space/PageController.java
@@ -20,12 +20,8 @@ public class PageController {
 		model.addAttribute("list", PageService.getPageList());
 		return "space/pageList";
 	}
-<<<<<<< HEAD
 	
-	@GetMapping("spacePage")
-=======
-	@GetMapping("page")
->>>>>>> 400a384649b00a5f12770eb2eef8aaa06c812b79
+	@GetMapping("/spacePage")
 	public String page() {
 		return "page/spacePage";
 	}
