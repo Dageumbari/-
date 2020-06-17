9d5d09ecf288ee7b189b3c6a1f05be9bfeb8a4ed
yanchu0 <dustn0514@gmail.com>
2020-06-17 20:42:35 +0900

    해당 css수정, 타임리프 수정

--- FinalProject/FinalProject/src/main/java/com/bit/model/dao/PageDAO.java
+++ FinalProject/FinalProject/src/main/java/com/bit/model/dao/PageDAO.java
@@ -3,11 +3,11 @@ package com.bit.model.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 import com.bit.model.vo.PageVO;
 
 @Mapper
 public interface PageDAO {
 	List<PageVO> getPageList();
+	List<PageVO> getAllList();
 }
